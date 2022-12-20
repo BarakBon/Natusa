b749027d6ee63487024c1834cb550b47f26c6242
BarakBon <bar14ak@gmail.com>
2022-12-20 14:26:05 +0200

    proper date minimum and ranges at search

--- Natusa/Scripts/SearchScript.js
+++ Natusa/Scripts/SearchScript.js
@@ -1,14 +1,22 @@
 ﻿$(document).ready(function () {
 
-    $(".returnInput").css({ "display": "none"});
-    
-})
+    $(".returnInput").css({ "display": "none" });
 
-$('input[name=wayRadio]').change(function () {
+    $("#depDate").attr({ "min": new Date().toJSON().split('T')[0] })
+
+    $("#depDate").change(function () {
+        $("#retDate").attr({ "min": $("#depDate").val() })
+    })
+
+
+    $('input[name=wayRadio]').change(function () {
         if ($(this).val() == "oneWay") { 
             $(".returnInput").fadeOut();
+            $("#retDate").val("");
+            $("#retTime").val("");
         }
         if ($(this).val() == "bothWay") {
             $(".returnInput").fadeIn();
         }
     })
+})
