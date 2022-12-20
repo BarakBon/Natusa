ff9da4aa7c0a71a4e5d0b8dc06cb3cc43128f8e5
BarakBon <bar14ak@gmail.com>
2022-12-20 14:01:48 +0200

    ways animation in search

---
+++ Natusa/Scripts/SearchScript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $(".returnInput").css({ "display": "none"});
+    
+})
+
+$('input[name=wayRadio]').change(function () {
+        if ($(this).val() == "oneWay") { 
+            $(".returnInput").fadeOut();
+        }
+        if ($(this).val() == "bothWay") {
+            $(".returnInput").fadeIn();
+        }
+    })
