f5c85ea941e4eb0430ca856c7cb0249d9eb1f435
BarakBon <bar14ak@gmail.com>
2022-12-24 08:57:28 +0200

    flight model and view model

---
+++ Natusa/Models/Flight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Natusa.Models
+{
+    public class Flight
+    {
+        [Key]
+        public String flightNum {get; set;}
+
+        public String origin { get; set;}
+
+        public String flightDate { get; set;} 
+
+        public String flightTime { get; set;}
+
+        public String destination { get; set;}
+
+        public int availableSeats { get; set;}                
+    }
+}
---
+++ Natusa/ViewModel/FlightsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Natusa.Models;
+
+namespace Natusa.ViewModel
+{
+    public class FlightsViewModel
+    {
+        public Flight flight { get; set; }
+
+        public List<Flight> flights { get; set;}
+    }
+}
